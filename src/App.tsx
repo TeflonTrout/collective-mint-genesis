5ee1a6e0e20ad6c4c8bae1743b26a1868a2e1781
TeflonTrout <jt@joy.world>
2025-05-19 10:41:02 -0400

    vercel for vite

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ import Footer from "./components/layout/Footer";
 import Campaign from "./pages/Campaign";
 import Profile from "./pages/Profile";
 import AdminPage from "./pages/Admin";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./polyfills.ts";
 
 const network = "https://api.devnet.solana.com";
